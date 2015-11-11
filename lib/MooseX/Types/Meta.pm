aea7896d5dab0cca87b36c472846193160eb4252
Karen Etheridge <ether@cpan.org>
2015-11-11 03:50:20 +0000

    Import of ETHER/MooseX-Types-Meta-0.02.tar.gz from CPAN.
    gitpan-cpan-distribution: MooseX-Types-Meta
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ETHER/MooseX-Types-Meta-0.02.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

--- lib/MooseX/Types/Meta.pm
+++ lib/MooseX/Types/Meta.pm
@@ -1,13 +1,10 @@
-package MooseX::Types::Meta;
-BEGIN {
-  $MooseX::Types::Meta::AUTHORITY = 'cpan:FLORA';
-}
-BEGIN {
-  $MooseX::Types::Meta::VERSION = '0.01';
-}
+use strict;
+use warnings;
+package MooseX::Types::Meta; # git description: 0.01-6-g107e523
 # ABSTRACT: Moose types to check against Moose's meta objects
 
-use Moose 1.05 ();
+our $VERSION = '0.02';
+
 use MooseX::Types -declare => [qw(
     TypeConstraint
     TypeCoercion
@@ -29,52 +26,141 @@ use MooseX::Types -declare => [qw(
 
 )];
 use Carp qw(confess);
-use namespace::clean;
+use if MooseX::Types->VERSION >= 0.42, 'namespace::autoclean';
+
+#pod =for :prelude
+#pod =for stopwords
+#pod ParameterizableRole
+#pod ParameterizedRole
+#pod RoleAttribute
+#pod StructuredTypeCoercion
+#pod StructuredTypeConstraint
+#pod TypeCoercion
+#pod TypeConstraint
+#pod
+#pod =cut
 
 # TODO: ParameterizedType{Constraint,Coercion} ?
 #       {Duck,Class,Enum,Parameterizable,Parameterized,Role,Union}TypeConstraint?
 
+#pod =type TypeConstraint
+#pod
+#pod A L<Moose::Meta::TypeConstraint>.
+#pod
+#pod =cut
 
 class_type TypeConstraint, { class => 'Moose::Meta::TypeConstraint' };
 
+#pod =type TypeCoercion
+#pod
+#pod A L<Moose::Meta::TypeCoercion>.
+#pod
+#pod =cut
 
 class_type TypeCoercion,   { class => 'Moose::Meta::TypeCoercion' };
 
+#pod =type Attribute
+#pod
+#pod A L<Class::MOP::Attribute>.
+#pod
+#pod =cut
 
 class_type Attribute,      { class => 'Class::MOP::Attribute' };
 
+#pod =type RoleAttribute
+#pod
+#pod A L<Moose::Meta::Role::Attribute>.
+#pod
+#pod =cut
 
 class_type RoleAttribute,  { class => 'Moose::Meta::Role::Attribute' };
 
+#pod =type Method
+#pod
+#pod A L<Class::MOP::Method>.
+#pod
+#pod =cut
 
 class_type Method,         { class => 'Class::MOP::Method' };
 
+#pod =type Class
+#pod
+#pod A L<Class::MOP::Class>.
+#pod
+#pod =cut
 
 class_type Class,          { class => 'Class::MOP::Class' };
 
+#pod =type Role
+#pod
+#pod A L<Moose::Meta::Role>.
+#pod
+#pod =cut
 
 class_type Role,           { class => 'Moose::Meta::Role' };
 
+#pod =type StructuredTypeConstraint
+#pod
+#pod A L<MooseX::Meta::TypeConstraint::Structured>.
+#pod
+#pod =cut
 
 class_type StructuredTypeConstraint, {
     class => 'MooseX::Meta::TypeConstraint::Structured',
 };
 
+#pod =type StructuredTypeCoercion
+#pod
+#pod A L<MooseX::Meta::TypeCoercion::Structured>.
+#pod
+#pod =cut
 
 class_type StructuredTypeCoercion, {
     class => 'MooseX::Meta::TypeCoercion::Structured',
 };
 
+#pod =type ParameterizableRole
+#pod
+#pod A L<MooseX::Role::Parameterized::Meta::Role::Parameterizable>.
+#pod
+#pod =cut
 
-class_type ParameterizableRole, {
-    class => 'MooseX::Role::Parameterized::Meta::Role::Parameterizable',
-};
+if (eval { require MooseX::Role::Parameterized; MooseX::Role::Parameterized->VERSION('1.03') }) {
+    role_type ParameterizableRole, {
+        role => 'MooseX::Role::Parameterized::Meta::Trait::Parameterizable',
+    };
+} else {
+    class_type ParameterizableRole, {
+        class => 'MooseX::Role::Parameterized::Meta::Role::Parameterizable',
+    };
+}
 
+#pod =type ParameterizedRole
+#pod
+#pod A L<MooseX::Role::Parameterized::Meta::Role::Parameterized>.
+#pod
+#pod =cut
 
 class_type ParameterizedRole, {
     class => 'MooseX::Role::Parameterized::Meta::Role::Parameterized',
 };
 
+#pod =type TypeEquals[`x]
+#pod
+#pod A L<Moose::Meta::TypeConstraint>, that's equal to the type constraint
+#pod C<x>.
+#pod
+#pod =type TypeOf[`x]
+#pod
+#pod A L<Moose::Meta::TypeConstraint>, that's either equal to or a subtype
+#pod of the type constraint C<x>.
+#pod
+#pod =type SubtypeOf[`x]
+#pod
+#pod A L<Moose::Meta::TypeConstraint>, that's a subtype of the type
+#pod constraint C<x>.
+#pod
+#pod =cut
 
 for my $t (
     [ 'TypeEquals', 'equals'        ],
@@ -101,16 +187,30 @@ for my $t (
     Moose::Util::TypeConstraints::add_parameterizable_type($tc);
 }
 
-__PACKAGE__->meta->make_immutable;
-
 1;
 
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
 MooseX::Types::Meta - Moose types to check against Moose's meta objects
+
+=head1 VERSION
+
+version 0.02
+
+=for stopwords
+ParameterizableRole
+ParameterizedRole
+RoleAttribute
+StructuredTypeCoercion
+StructuredTypeConstraint
+TypeCoercion
+TypeConstraint
 
 =head1 TYPES
 
@@ -173,11 +273,28 @@ of the type constraint C<x>.
 A L<Moose::Meta::TypeConstraint>, that's a subtype of the type
 constraint C<x>.
 
+=head1 SUPPORT
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=MooseX-Types-Meta>
+(or L<bug-MooseX-Types-Meta@rt.cpan.org|mailto:bug-MooseX-Types-Meta@rt.cpan.org>).
+
+There is also a mailing list available for users of this distribution, at
+L<http://lists.perl.org/list/moose.html>.
+
+There is also an irc channel available for users of this distribution, at
+irc://irc.perl.org/#moose.
+
 =head1 AUTHOR
 
-  Florian Ragwitz <rafl@debian.org>
+Florian Ragwitz <rafl@debian.org>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 CONTRIBUTOR
+
+=for stopwords Karen Etheridge
+
+Karen Etheridge <ether@cpan.org>
+
+=head1 COPYRIGHT AND LICENCE
 
 This software is copyright (c) 2010 by Florian Ragwitz.
 
@@ -185,4 +302,3 @@ This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
